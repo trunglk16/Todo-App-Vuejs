daa3b89ba9ce357d4d177b906bc44c09da1a454b
Trong Trung Nguyen <trung_nt@septeni-technology.jp>
2019-07-08 16:56:07 +0700

    first

---
+++ src/components/TodoList.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" v-model="newTodo" placeholder="todo list" @keyup.enter="addTodo()">
+    <div v-for="todo in todos" v-bind:key="todo.id" class="todo-item">{{todo.title}}</div>
+  </div>
+
+</template>
+
+<script>
+ export default {
+    name: 'TodoList',
+    data () {
+      return {
+        newTodo: '',
+        idForTodo: 3,
+        todos: [
+          {
+            'id':1,
+            'title': 'Finish Vue Screencast',
+            'completed': false,
+          },
+          {
+            'id':2,
+            'title': 'Take over world',
+            'completed': false,
+          },
+        ],
+        content: "love ya"
+      }
+    },
+   methods: {
+      addTodo() {
+        this.todos.push({
+          id: this.idForTodo,
+          title: this.newTodo,
+          completed: false,
+        })
+
+        this.newTodo = ''
+        this.idForTodo++
+      }
+   }
+  }
+
+</script>
+
+<style scoped>
+
+</style>
