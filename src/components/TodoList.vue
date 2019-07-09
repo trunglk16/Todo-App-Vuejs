662666ad0a393450c1fb3d8c2b3979c99b8aae45
Trong Trung Nguyen <trung_nt@septeni-technology.jp>
2019-07-09 08:44:25 +0700

    basic list todo, animation. edit, remove, create todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <input type="text" class="todo-input" v-model="newTodo" placeholder="List to do!!!!!" @keyup.enter="addTodo()">
-    <div v-for="(todo, index) in todos" v-bind:key="todo.id" class="todo-item">
+    <transition-group name="fade" enter-active-class="animated fadeInUp" leave-active-class="animated fadeOutDown">
+    <div v-for="(todo, index) in todosFiltered" v-bind:key="todo.id" class="todo-item">
       <div class="todo-item-left">
         <input @click="status(todo)" type="checkbox" v-model="todo.completed">
         <div v-if="!todo.editing" :class="{completed : todo.completed}"  @dblclick="editTodo(todo)" class="todo-item-label">{{todo.title}}</div>
@@ -10,44 +11,79 @@
       <div @click="removeTodo(index)" class="remove-item">&times;
       </div>
     </div>
+    </transition-group>
     <div class="extra-container">
       <div>
         <label>
-          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos"> Check All
+          <input type="checkbox" :checked="!anyRemaining" @change="checkAllTodos()"> Check All
         </label>
       </div>
       {{remaining}} items left
     </div>
+
+    <div class="extra-container">
+      <div>
+        <button :class="{ active: filter === 'all' }" @click="filter = 'all'">All</button>
+        <button :class="{ active: filter === 'active' }" @click="filter = 'active'"> Active</button>
+        <button :class="{ active: filter === 'completed' }" @click="filter = 'completed'"> Completed</button>
+      </div>
+      <div>
+        <transition name="fade">
+          <button v-if="showClearCompletedButton" @click="clearCompleted()">Clear completed</button>
+        </transition>
+      </div>
+    </div>
+
   </div>
+
 
 </template>
 
 <script>
  export default {
-    name: 'TodoList',
-    data () {
-      return {
-        newTodo: '',
-        idForTodo: 3,
-        beforeEditCache:'',
-        todos: [
-          {
-            'id':1,
-            'title': 'Finish Vue Screencast',
-            'completed': true,
-            'editing': false
-          },
-          {
-            'id':2,
-            'title': 'Take over world',
-            'completed': false,
-            'editing': false
+   name: 'TodoList',
+   data() {
+     return {
+       newTodo: '',
+       idForTodo: 3,
+       beforeEditCache: '',
+       filter: 'all',
+       todos: [
+         {
+           'id': 1,
+           'title': 'Finish Vue Screencast',
+           'completed': true,
+           'editing': false
+         },
+         {
+           'id': 2,
+           'title': 'Take over world',
+           'completed': false,
+           'editing': false
 
-          },
-        ],
-        content: "love ya"
-      }
-    },
+         },
+       ],
+       content: "love ya"
+     }
+   },
+   computed: {
+     remaining() {
+       return this.todos.filter(todo => !todo.completed).length
+     },
+     anyRemaining() {
+       return this.remaining !== 0
+     },
+     todosFiltered() {
+       if(this.filter === 'all')
+         return this.todos
+       else if ( this.filter === 'active')
+         return  this.todos.filter(todo => !todo.completed)
+       else return this.todos.filter(todo => todo.completed)
+     },
+     showClearCompletedButton(){
+       return this.todos.filter(todo => todo.completed).length > 0
+     }
+   },
    directives: {
      focus: {
        inserted: function (el) {
@@ -56,59 +92,71 @@
      }
    },
    methods: {
-      addTodo() {
-        if (this.newTodo.trim().length === 0) {
-          return
-        }
-        this.todos.push({
-          id: this.idForTodo,
-          title: this.newTodo,
-          completed: false,
-          editing: false
-        })
+     addTodo() {
+       if (this.newTodo.trim().length === 0) {
+         return
+       }
+       this.todos.push({
+         id: this.idForTodo,
+         title: this.newTodo,
+         completed: false,
+         editing: false
+       })
 
-        this.newTodo = ''
-        this.idForTodo++
-      },
+       this.newTodo = ''
+       this.idForTodo++
+     },
 
-      removeTodo(index){
-        this.todos.splice(index,1)
-      },
+     removeTodo(index) {
+       this.todos.splice(index, 1)
+     },
 
      editTodo(todo) {
-          this.beforeEditCache = todo.title
-          todo.editing = true
+       this.beforeEditCache = todo.title
+       todo.editing = true
      },
 
      doneEdit(todo) {
-        if(todo.title.length === 0) {
-          todo.title = this.beforeEditCache
-        }
+       if (todo.title.length === 0) {
+         todo.title = this.beforeEditCache
+       }
        todo.editing = false
      },
 
-     cancelEdit(todo){
-        todo.title = this.beforeEditCache
-        todo.editing = false
+     cancelEdit(todo) {
+       todo.title = this.beforeEditCache
+       todo.editing = false
      },
 
-     status(todo){
-        if(todo.completed) {
-          todo.completed = false
-          console.log(todo.completed)
-        }
-       else  {
+     status(todo) {
+       if (todo.completed) {
+         todo.completed = false
+         console.log(todo.completed)
+       } else {
          todo.completed = true
-          console.log(todo.completed)
+         console.log(todo.completed)
        }
 
+     },
+
+     checkAllTodos() {
+       this.todos.map(todo => todo.completed = event.target.checked)
+       console.log(this.todos)
+     },
+
+     clearCompleted(){
+       this.todos = this.todos.filter(todo => !todo.completed)
      }
+
+
    }
-  }
+ }
 
 </script>
 
 <style lang="scss">
+  @import url("https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css")
+  ;
   .todo-input {
     width: 100%;
     padding: 10px 18px;
@@ -125,6 +173,7 @@
     display: flex;
     align-items: center;
     justify-content: space-between;
+    animation-duration: 0.3s;
   }
 
   .remove-item {
@@ -193,6 +242,10 @@
 
   .active {
     background: lightgreen;
+  }
+
+  .fade-enter-active, .fade-leave-active {
+    transition: opacity .2s;
   }
 
 
